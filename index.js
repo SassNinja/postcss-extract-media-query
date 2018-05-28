fa8ea3dc7120b877223fe54a68f5e78e8bbf5b63
SassNinja <kai.falkowski@gmail.com>
2018-05-28 17:37:06 +0200

    work over whole extraction code & add stats option
    Instead of using confusing 'css, res' I'm using now more familiar 'root, result'. Apart from that the logic is now more stable & reliable due to the usage of clearer atRule, rule and root variables.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const _ = require('lodash');
 const fs = require('fs-extra');
 const path = require('path');
+const chalk = require('chalk');
 const postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-extract-media-query', opts => {
@@ -12,50 +13,78 @@ module.exports = postcss.plugin('postcss-extract-media-query', opts => {
             name: '[name]-[query].[ext]'
         },
         queries: {},
-        combine: true
+        combine: true,
+        stats: true
     }, opts);
 
-    function addToCss(css, key, val) {
-        if (!css[key]) {
-            css[key] = postcss.parse(val);
-            val.remove();
-        } else {
-            css[key].append(val);
+    function addToAtRules(atRules, key, atRule) {
+
+        // init array for target key if undefined
+        if (!atRules[key]) {
+            atRules[key] = [];
         }
+
+        // create new atRule if none existing or combine false
+        if (atRules[key].length < 1 || opts.combine === false) {
+            atRules[key].push(postcss.atRule({ name: atRule.name, params: atRule.params }));
+        }
+
+        // pointer to last item in array
+        const lastAtRule = atRules[key][atRules[key].length - 1];
+
+        // append all rules
+        atRule.walkRules(rule => {
+            lastAtRule.append(rule);
+        });
+
+        // remove atRule from original chunk
+        atRule.remove();
     }
 
-    return (css, res) => {
+    return (root, result) => {
 
-        const file = res.opts.from.match(/[^/\\]+\.\w+$/)[0].split('.');
+        const file = result.opts.from.match(/[^/\\]+\.\w+$/)[0].split('.');
         const name = file[0];
         const ext = file[1];
 
-        const newCss = {};
+        const newAtRules = {};
 
-        css.walkAtRules(rule => {
-            if (rule.name === 'media') {
+        root.walkAtRules('media', atRule => {
 
-                // use custom query name if available (e.g. tablet)
-                // or otherwise the query key (converted to kebab case)
-                const key = typeof opts.queries[rule.params] === 'string'
-                            ? opts.queries[rule.params] 
-                            : _.kebabCase(rule.params);
+            // use custom query name if available (e.g. tablet)
+            // or otherwise the query key (converted to kebab case)
+            const key = typeof opts.queries[atRule.params] === 'string'
+                        ? opts.queries[atRule.params] 
+                        : _.kebabCase(atRule.params);
 
-                // extract media rule
-                addToCss(newCss, key, rule);
-            }
+            // extract media atRule
+            // and concatenate with existing atRule (same key)
+            addToAtRules(newAtRules, key, atRule);
         });
 
-        Object.keys(newCss).forEach(key => {
-            
+        Object.keys(newAtRules).forEach(key => {
+
             const newFile = opts.output.name
                                 .replace('[name]', name)
                                 .replace('[query]', key)
                                 .replace('[ext]', ext);
+
             const newFilePath = path.join(opts.output.path, newFile);
 
+            // create new root
+            // and append all extracted atRules with current key
+            const newRoot = postcss.root();
+            newAtRules[key].forEach(newAtRule => {
+                newRoot.append(newAtRule);
+            });
+
             // emit extracted css file
-            fs.outputFile(newFilePath, newCss[key])
+            fs.outputFile(newFilePath, newRoot.toString())
+                .then(() => {
+                    if (opts.stats === true) {
+                        console.log(chalk.green('[extracted media query]'), newFile);
+                    }
+                })
                 .catch(err => {
                     console.error(err);
                 });
