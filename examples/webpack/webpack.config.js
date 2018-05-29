9f12896144713be5a69c47a41dd3a977a4af8695
SassNinja <kai.falkowski@gmail.com>
2018-05-29 18:04:51 +0200

    add complete example for webpack

---
+++ examples/webpack/webpack.config.js
@@ -0,0 +1,34 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        example: './src/example.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ]
+};
