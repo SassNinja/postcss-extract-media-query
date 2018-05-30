5b9fc6c6219048c13c315dbb15edd6f4cac63c0d
SassNinja <kai.falkowski@gmail.com>
2018-05-30 11:24:26 +0200

    add complete example for gulp

---
+++ examples/gulp/gulpfile.js
@@ -0,0 +1,21 @@
+
+const path = require('path');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const extractMediaQuery = require('postcss-extract-media-query');
+
+function clean() {
+    return gulp.src(path.join(__dirname, 'dist/*'))
+        .pipe($.deleteFile());
+}
+
+function css() {
+    return gulp.src(path.join(__dirname, 'src/*.css'))
+        .pipe($.postcss())
+        .pipe(gulp.dest(path.join(__dirname, 'dist')));
+}
+
+gulp.task('default', gulp.series(
+    clean,
+    css
+));
---
+++ examples/gulp/postcss.config.js
@@ -0,0 +1,16 @@
+
+const path = require('path');
+
+module.exports = {
+    plugins: {
+        'postcss-extract-media-query': {
+            output: {
+                path: path.join(__dirname, 'dist')
+            },
+            queries: {
+                'screen and (min-width: 1024px)': 'desktop'
+            },
+            stats: false
+        }
+    }
+};
