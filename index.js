0352d5bc4887e216922c1a124b68b398c4efb974
fabs <faebeee@gmail.com>
2019-03-20 16:16:08 +0100

    Reapply indentions

--- index.js
+++ index.js
@@ -82,9 +82,9 @@ module.exports = postcss.plugin('postcss-extract-media-query', opts => {
             if (opts.output.path) {
 
                 const newFile = opts.output.name
-                        .replace(/\[name\]/g, name)
-                        .replace(/\[query\]/g, key)
-                        .replace(/\[ext\]/g, ext)
+                                .replace(/\[name\]/g, name)
+                                .replace(/\[query\]/g, key)
+                                .replace(/\[ext\]/g, ext)
 
                 const newFilePath = path.join(opts.output.path, newFile);
 
@@ -97,8 +97,8 @@ module.exports = postcss.plugin('postcss-extract-media-query', opts => {
 
                 if (opts.minimize === true) {
                     const newRootMinimized = postcss([ csswring() ])
-                                .process(newRoot.toString(), { from: newFilePath })
-                                .root;
+                                                .process(newRoot.toString(), { from: newFilePath })
+                                                .root;
                     fs.outputFileSync(newFilePath, newRootMinimized.toString());
                 } else {
                     fs.outputFileSync(newFilePath, newRoot.toString());
