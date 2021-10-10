e4af958c4e07c4627f8448babc49ad6f7d79195f
Kai Falkowski <kai.falkowski@gmail.com>
2021-10-10 16:17:27 +0200

    Introduce prettifier & run for all code (#36)

--- examples/gulp/postcss.config.js
+++ examples/gulp/postcss.config.js
@@ -1,15 +1,14 @@
-
 const path = require('path');
 
 module.exports = {
-    plugins: {
-        'postcss-extract-media-query': {
-            output: {
-                path: path.join(__dirname, 'dist')
-            },
-            queries: {
-                'screen and (min-width: 1024px)': 'desktop'
-            }
-        }
-    }
+  plugins: {
+    'postcss-extract-media-query': {
+      output: {
+        path: path.join(__dirname, 'dist'),
+      },
+      queries: {
+        'screen and (min-width: 1024px)': 'desktop',
+      },
+    },
+  },
 };
--- examples/parcel/parcel.js
+++ examples/parcel/parcel.js
@@ -1,12 +1,11 @@
-
 const parcel = require('parcel-bundler');
 const path = require('path');
 
 const file = path.join(__dirname, 'src/example.js');
 
 const options = {
-    watch: false,
-    sourceMaps: false
+  watch: false,
+  sourceMaps: false,
 };
 
 const bundler = new parcel(file, options);
